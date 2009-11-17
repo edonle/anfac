9fa7c9df5e2463303ef9860bfd3c723dcf7ef1f5
edonle-anfac <edonle@gmail.com>
2009-11-17 17:57:35 -0200

    Criando testes para o model [Fator] e atributo [prazo]

--- app/models/fator.rb
+++ app/models/fator.rb
@@ -1,2 +1,4 @@
 class Fator < ActiveRecord::Base
+  validates_presence_of :prazo, :message => "não pode ser nulo"
+  validates_numericality_of :prazo, :greater_than_or_equal_to => 1, :only_integer => true, :message => "não é um número válido"
 end
--- spec/models/fator_spec.rb
+++ spec/models/fator_spec.rb
@@ -1,13 +1,52 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+module FatorSpecHelper
+  def valid_fator_attributes
+    { :prazo => 30 }
+  end
+end
+
 describe Fator do
+  
+  include FatorSpecHelper
+  
   before(:each) do
-    @valid_attributes = {
-      :prazo => 1
-    }
+    @fator = Fator.new
   end
 
-  it "should create a new instance given valid attributes" do
-    Fator.create!(@valid_attributes)
+  it "deve ser válido" do
+    @fator.attributes = valid_fator_attributes
+    @fator.should be_valid
   end
+
+  # Atributo: prazo
+
+  it "prazo não pode ser nulo" do 
+    @fator.attributes = valid_fator_attributes.except(:prazo)
+    @fator.prazo = nil
+    @fator.should_not be_valid
+    @fator.errors.on(:prazo).should include("não pode ser nulo")
+  end  
+
+  it "prazo deve ser maior que zero" do
+    @fator.attributes = valid_fator_attributes.except(:prazo)
+    @fator.prazo = 0
+    @fator.should_not be_valid
+    @fator.errors.on(:prazo).should include("não é um número válido")
+  end
+
+  it "prazo deve ser numero inteiro" do
+    @fator.attributes = valid_fator_attributes.except(:prazo)
+    @fator.prazo = 1.1
+    @fator.should_not be_valid
+    @fator.errors.on(:prazo).should include("não é um número válido")
+  end
+
+  it "prazo deve ser numero" do
+    @fator.attributes = valid_fator_attributes.except(:prazo)
+    @fator.prazo = "a"
+    @fator.should_not be_valid
+    @fator.errors.on(:prazo).should include("não é um número válido")
+  end
+
 end
